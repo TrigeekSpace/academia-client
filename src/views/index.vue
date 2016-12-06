a06d60843c9cc03baf2bb97f2ca86d988074c0b1
Mark Lv <lmy1229@126.com>
2016-12-06 20:35:52 +0800

    add user/space.vue

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,11 +1,33 @@
 <template>
-<h1>Hello world!<h1>
+    <div>
+        <div class="row">
+            <div class="col-sm-2 col-md-3 col-lg-3"></div>
+            <div class="col-sm-8 col-md-6 col-lg-6">
+                <div align="center">
+                    <h1 class="big-title">Academia Search</h1>
+                    <div class="input-group">
+                        <input type="text" class="form-control" placeholder="在Academia上搜索论文" v-model="search_text">
+                        <span class="input-group-btn">
+                            <router-link class="btn btn-default" to="/papers/list?query={{encodeURIComponent(search_text)}}">
+                                Search!
+                            </router-link>
+                        </span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-2 col-md-3 col-lg-3"></div>
+        </div>
+    </div>
 </template>
 
 <script>
 export default {
-
-}
+    data() {
+        return {
+            search_text: ""
+        };
+    }
+};
 </script>
 
 <style>
---
+++ src/views/users/space.vue
@@ -0,0 +1,29 @@
+<!-- component template -->
+<template>
+<div>
+    <div class="row">
+        <div class="col-sm-2 col-md-2 col-lg-2"></div>
+        <div class="col-sm-8 col-md-8 col-lg-8">
+
+
+        </div>
+        <div class="col-sm-2 col-md-2 col-lg-2"></div>
+    </div>
+</div>
+</template>
+<!-- script -->
+<script>
+export default {
+    data()
+    {
+        return {
+            user: null,
+            papers: null
+        }
+    },
+    ready()
+    {
+
+    }
+};
+</script>
