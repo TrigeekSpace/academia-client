1d34317f7d44e97c33f2ee6a4cceea6eea2d33f9
Lustralisk <wzlsc_lustre@163.com>
2016-12-17 15:57:00 +0800

    fix menu back to index bug

--- src/views/root.vue
+++ src/views/root.vue
@@ -9,7 +9,7 @@
             </div>
             <!-- Menu -->
             <ul class="nav navbar-nav">
-                <li class="active" href="javascript:void(0)" @click="$root.toggle_sidebar()" id="menu-button"><a href="#"><span class="glyphicon glyphicon-list"></span>&nbsp;&nbsp;Menu</a></li>
+                <li class="active" href="javascript:void(0)" @click="$root.toggle_sidebar()" id="menu-button"><a><span class="glyphicon glyphicon-list"></span>&nbsp;&nbsp;Menu</a></li>
                 <li id="back-button"><a href="#"><span class="glyphicon glyphicon-chevron-left"></span></a></li>
                 <li id="forward-button"><a href="#"><span class="glyphicon glyphicon-chevron-right"></span></a></li>
             </ul>
