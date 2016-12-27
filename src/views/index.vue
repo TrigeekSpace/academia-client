ba07eea1061c6f9cbfaaa35f5e09000b764e63a1
Mark Lv <lmy1229@126.com>
2016-12-27 21:05:00 +0800

    minor fix

--- src/views/index.vue
+++ src/views/index.vue
@@ -16,26 +16,6 @@
             </div>
         </div>
         <div class="col-sm-2 col-md-3 col-lg-3"></div>
-  <div>
-    <div class="row center-vertical">
-      <div class="col-sm-2 col-md-3 col-lg-3"></div>
-      <div class="col-sm-8 col-md-6 col-lg-6">
-        <div align="center">
-          <h1 class="big-title">Academia Search</h1>
-          <br>
-          <div class="input-group">
-            <input type="text" class="form-control" id="search_input" :placeholder="language.search" v-model="search_text" data-toggle="popover" data-placement="bottom" :data-content="language.lack_of_word" data-trigger="manual" v-on:keyup.enter="search()">
-            <span class="input-group-btn">
-              <button id="search-btn" class="btn btn-default" @click="search()">
-                Search!
-              </button>
-            </span>
-          </div>
-        </div>
-      </div>
-      <div class="col-sm-2 col-md-3 col-lg-3"></div>
->>>>>>> dc224b8490cbcda92b83257ff05de691842d10db
-    </div>
   </div>
 </template>
 
