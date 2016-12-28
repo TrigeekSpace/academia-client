a87d4379adf426566f0e324e4c466c8445ae3758
Lustralisk <wzlsc_lustre@163.com>
2016-12-28 08:03:52 +0800

    upload paper language finished

--- src/views/notes/upload.vue
+++ src/views/notes/upload.vue
@@ -1,44 +1,44 @@
 <!-- Component template -->
 <template>
 <div>
-    <div class="row">
-        <div class="hidden-sm hidden-md col-lg-1"></div>
-        <div class="col-sm-12 col-md-12 col-lg-10">
-            <div>
-                <h1>{{language.upload_note}}</h1>
-                <hr />
-                <form @submit.prevent>
-                    <!-- Note title -->
-                    <div class="form-group">
-                        <label>{{language.note_title}}</label>
-                        <input type="text" class="form-control" :placeholder="language.note_title" v-model="note_title" />
-                    </div>
-                    <!-- Related paper title -->
-                    <div class="form-group">
-                        <label>{{language.relate_paper}}</label>
-                        <input disabled="disable" type="text" class="form-control" :placeholder="language.relate_paper" v-model="paper_title" />
-                    </div>
-                    <!-- Note content -->
-                    <div class="form-group">
-                        <label>{{language.note_content}}</label>
-                        <textarea id="note-content"></textarea>
-                    </div>
-                    <!-- Annotation file -->
-                    <div class="form-group">
-                        <label>{{language.annotation}}</label>
-                        <input type="file" id="file-selector" />
-                    </div>
-                    <!-- Note operations -->
-                    <div class="form-group" align="right">
-                        <button class="btn btn-primary" @click="update_note()" v-if="update_mode">{{language.save}}</button>&nbsp;
-                        <button id="note_upload" class="btn btn-primary" @click="create_note()" v-if="!update_mode">{{language.publish}}</button>&nbsp;
-                        <button class="btn btn-default" @click="$router.back()">{{language.cancel}}</button>
-                    </div>
-                </form>
-            </div>
-        </div>
-        <div class="hidden-sm hidden-md col-lg-1"></div>
+  <div class="row">
+    <div class="hidden-sm hidden-md col-lg-1"></div>
+    <div class="col-sm-12 col-md-12 col-lg-10">
+      <div>
+        <h1>{{language.upload_note}}</h1>
+        <hr />
+        <form @submit.prevent>
+          <!-- Note title -->
+          <div class="form-group">
+            <label>{{language.note_title}}</label>
+            <input type="text" class="form-control" :placeholder="language.note_title" v-model="note_title" />
+          </div>
+          <!-- Related paper title -->
+          <div class="form-group">
+            <label>{{language.relate_paper}}</label>
+            <input disabled="disable" type="text" class="form-control" :placeholder="language.relate_paper" v-model="paper_title" />
+          </div>
+          <!-- Note content -->
+          <div class="form-group">
+            <label>{{language.note_content}}</label>
+            <textarea id="note-content"></textarea>
+          </div>
+          <!-- Annotation file -->
+          <div class="form-group">
+            <label>{{language.annotation}}</label>
+            <input type="file" id="file-selector" />
+          </div>
+          <!-- Note operations -->
+          <div class="form-group" align="right">
+            <button class="btn btn-primary" @click="update_note()" v-if="update_mode">{{language.save}}</button>&nbsp;
+            <button id="note_upload" class="btn btn-primary" @click="create_note()" v-if="!update_mode">{{language.publish}}</button>&nbsp;
+            <button class="btn btn-default" @click="$router.back()">{{language.cancel}}</button>
+          </div>
+        </form>
+      </div>
     </div>
+    <div class="hidden-sm hidden-md col-lg-1"></div>
+  </div>
 </div>
 </template>
 <!-- Script -->
@@ -53,187 +53,187 @@ import {unify_error} from "academia/util/error";
 import {msgbox} from "academia/util/core";
 
 export default {
-    //View data
-    data()
-    {   return {
-            //Note data
-            note_title: "",
-            paper_title: "",
-            //Update mode
-            update_mode: false,
-            language: {
-                note_title: "",
-                relate_paper: ""
-            }
+  //View data
+  data()
+  {   return {
+      //Note data
+      note_title: "",
+      paper_title: "",
+      //Update mode
+      update_mode: false,
+      language: {
+        note_title: "",
+        relate_paper: ""
+      }
+    };
+  },
+  beforeRouteEnter: pre_route(login_required),
+  //Methods
+  methods: {
+    async init()
+    {   //Note and paper ID
+      let paper_id = parseInt(this.$route.query.paper_id);
+      let note_id = parseInt(this.$route.query.note_id);
+      //Update mode
+      this.update_mode = !isNaN(note_id);
+
+      //Paper
+      this._paper = await Paper.find(paper_id);
+      //Paper title
+      this.paper_title = this._paper.title;
+
+      //Editor
+      this.editor = new SimpleMDE({
+        element: $("#note-content", this.$root.$el)[0],
+        spellChecker: false
+      });
+
+      //Fetch note in update mode
+      if (this.update_mode)
+      {   let note = this._note = await Note.find(note_id);
+        //Set note title and content
+        this.note_title = note.title;
+        this.note_content = note.content;
+      }
+      // Setting language
+      let lang = this.$root.settings.lang;
+      this.language.upload_note = lang == '#langCN' ? '上传笔记' : 'Upload Note';
+      this.language.note_title = lang == '#langCN' ? '笔记名称' : 'Note title';
+      this.language.relate_paper = lang == '#langCN' ? '相关论文' : 'Related Paper';
+      this.language.note_content = lang == '#langCN' ? '笔记内容' : 'Note Content';
+      this.language.annotation = lang == '#langCN' ? '批注论文' : 'Annotation';
+      this.language.save = lang == '#langCN' ? '保存' : 'Save';
+      this.language.publish = lang == '#langCN' ? '发布' : 'Publish';
+      this.language.cancel = lang == '#langCN' ? '取消' : 'Cancel';
+    },
+    //Update note
+    async update_note()
+    {   try
+      {   //Update data
+        let update_data = {
+          paper: this._paper.id,
+          title: this.note_title,
+          content: this.note_content
         };
-    },
-    beforeRouteEnter: pre_route(login_required),
-    //Methods
-    methods: {
-        async init()
-        {   //Note and paper ID
-            let paper_id = parseInt(this.$route.query.paper_id);
-            let note_id = parseInt(this.$route.query.note_id);
-            //Update mode
-            this.update_mode = !isNaN(note_id);
+        //Update annotation file
+        let file_selector = $("#file-selector", this.$root.$el)[0];
+        if (file_selector.files.length>0)
+          update_data.annotation_file = file_selector.files[0];
 
-            //Paper
-            this._paper = await Paper.find(paper_id);
-            //Paper title
-            this.paper_title = this._paper.title;
-
-            //Editor
-            this.editor = new SimpleMDE({
-                element: $("#note-content", this.$root.$el)[0],
-                spellChecker: false
-            });
-
-            //Fetch note in update mode
-            if (this.update_mode)
-            {   let note = this._note = await Note.find(note_id);
-                //Set note title and content
-                this.note_title = note.title;
-                this.note_content = note.content;
-            }
-            // Setting language
-            let lang = this.$root.settings.lang;
-            this.language.upload_note = lang == '#langCN' ? '上传笔记' : 'Upload Note';
-            this.language.note_title = lang == '#langCN' ? '笔记名称' : 'Note title';
-            this.language.relate_paper = lang == '#langCN' ? '相关论文' : 'Related Paper';
-            this.language.note_content = lang == '#langCN' ? '笔记内容' : 'Note Content';
-            this.language.annotation = lang == '#langCN' ? '批注论文' : 'Annotation';
-            this.language.save = lang == '#langCN' ? '保存' : 'Save';
-            this.language.publish = lang == '#langCN' ? '发布' : 'Publish';
-            this.language.cancel = lang == '#langCN' ? '取消' : 'Cancel';
-        },
-        //Update note
-        async update_note()
-        {   try
-            {   //Update data
-                let update_data = {
-                    paper: this._paper.id,
-                    title: this.note_title,
-                    content: this.note_content
-                };
-                //Update annotation file
-                let file_selector = $("#file-selector", this.$root.$el)[0];
-                if (file_selector.files.length>0)
-                    update_data.annotation_file = file_selector.files[0];
-
-                //Create upload transfer task
-                let new_upload_task = {
-                    name: this.note_title,
-                    type: "笔记",
-                    progress: 0,
-                    transfered: 0,
-                    total: 1
-                }
-                this.$root.upload_tasks.push(new_upload_task);
-
-                //Do update
-                this._note.DSUpdate(update_data, {
-                    method: "PATCH",
-                    onUploadProgress: progress_listener(new_upload_task)
-                }).then(() => {
-                    _.pull(this.$root.upload_tasks, new_upload_task);
-                });
-                //Go to note detail page
-                this.$router.push({
-                    name: "paper_content",
-                    query: {
-                        paper_id: this._paper.id
-                    }
-                });
-            }
-            catch (e)
-            {   e = unify_error(e);
-                //Prompt error
-                msgbox({
-                    type: "error",
-                    title: "无法上传笔记",
-                    message: e.message
-                });
-            }
-        },
-        //Create note
-        async create_note()
-        {   if ((!this.note_title) || (!this.note_content))
-            {   msgbox({
-                    type: "error",
-                    title: "无法上传笔记",
-                    message: "您尚未填写笔记标题或内容。"
-                });
-                return;
-            }
-
-            let file_selector = $("#file-selector", this.$root.$el)[0];
-            //Must select a file
-            if (file_selector.files.length<1)
-            {   msgbox({
-                    type: "error",
-                    title: "无法上传笔记",
-                    message: "您尚未选择批注论文文件。"
-                });
-                return;
-            }
-
-            //Create upload transfer task
-            let new_upload_task = {
-                name: this.note_title,
-                type: "笔记",
-                progress: 0,
-                transfered: 0,
-                total: 1
-            }
-            this.$root.upload_tasks.push(new_upload_task);
-
-            try
-            {   //Create note
-                Note.create({
-                    paper: this._paper.id,
-                    title: this.note_title,
-                    content: this.note_content,
-                    annotation_file: file_selector.files[0]
-                }, {
-                    onUploadProgress: progress_listener(new_upload_task)
-                }).then(() => {
-                    //Remove upload task
-                    _.pull(this.$root.upload_tasks, new_upload_task);
-                });
-
-                //Go to note detail page
-                this.$router.push({
-                    name: "index",
-                    query: {
-                        paper_id: this._paper.id
-                    }
-                });
-            }
-            catch (e)
-            {   e = unify_error(e);
-                //Prompt error
-                msgbox({
-                    type: "error",
-                    title: "无法上传笔记",
-                    message: e.toString()
-                });
-            }
+        //Create upload transfer task
+        let new_upload_task = {
+          name: this.note_title,
+          type: "笔记",
+          progress: 0,
+          transfered: 0,
+          total: 1
         }
+        this.$root.upload_tasks.push(new_upload_task);
+
+        //Do update
+        this._note.DSUpdate(update_data, {
+          method: "PATCH",
+          onUploadProgress: progress_listener(new_upload_task)
+        }).then(() => {
+          _.pull(this.$root.upload_tasks, new_upload_task);
+        });
+        //Go to note detail page
+        this.$router.push({
+          name: "paper_content",
+          query: {
+            paper_id: this._paper.id
+          }
+        });
+      }
+      catch (e)
+      {   e = unify_error(e);
+        //Prompt error
+        msgbox({
+          type: "error",
+          title: "无法上传笔记",
+          message: e.message
+        });
+      }
     },
-    watch: {
-        $route: on_route_change
-    },
-    //Computed properties
-    computed: {
-        note_content: {
-            get()
-            {   return this.editor?this.editor.value():null;
-            },
-            set(markdown)
-            {   if (this.editor)
-                    this.editor.value(markdown);
-            }
-        }
+    //Create note
+    async create_note()
+    {   if ((!this.note_title) || (!this.note_content))
+      {   msgbox({
+          type: "error",
+          title: "无法上传笔记",
+          message: "您尚未填写笔记标题或内容。"
+        });
+        return;
+      }
+
+      let file_selector = $("#file-selector", this.$root.$el)[0];
+      //Must select a file
+      if (file_selector.files.length<1)
+      {   msgbox({
+          type: "error",
+          title: "无法上传笔记",
+          message: "您尚未选择批注论文文件。"
+        });
+        return;
+      }
+
+      //Create upload transfer task
+      let new_upload_task = {
+        name: this.note_title,
+        type: "笔记",
+        progress: 0,
+        transfered: 0,
+        total: 1
+      }
+      this.$root.upload_tasks.push(new_upload_task);
+
+      try
+      {   //Create note
+        Note.create({
+          paper: this._paper.id,
+          title: this.note_title,
+          content: this.note_content,
+          annotation_file: file_selector.files[0]
+        }, {
+          onUploadProgress: progress_listener(new_upload_task)
+        }).then(() => {
+          //Remove upload task
+          _.pull(this.$root.upload_tasks, new_upload_task);
+        });
+
+        //Go to note detail page
+        this.$router.push({
+          name: "index",
+          query: {
+            paper_id: this._paper.id
+          }
+        });
+      }
+      catch (e)
+      {   e = unify_error(e);
+        //Prompt error
+        msgbox({
+          type: "error",
+          title: "无法上传笔记",
+          message: e.toString()
+        });
+      }
     }
+  },
+  watch: {
+    $route: on_route_change
+  },
+  //Computed properties
+  computed: {
+    note_content: {
+      get()
+      {   return this.editor?this.editor.value():null;
+      },
+      set(markdown)
+      {   if (this.editor)
+          this.editor.value(markdown);
+      }
+    }
+  }
 };
 </script>
--- src/views/papers/upload.vue
+++ src/views/papers/upload.vue
@@ -1,72 +1,72 @@
 <template>
 <div>
-    <div class="row">
-        <div class="col-sm-1 col-md-1 col-lg-1"></div>
-        <div class="col-sm-10 col-md-10 col-lg-10">
-            <h1>上传论文</h1>
-            <hr />
-            <form class="form-horizontal" role="form" @submit.prevent>
-                <!-- Paper title -->
-                <div class="form-group form-group-padding-fixes">
-                    <label>论文标题</label>
-                    <input type="text" class="form-control" id="paper_title" placeholder="论文标题" v-model="title">
-                </div>
-                <!-- Paper authors -->
-                <div class="form-group form-group-padding-fixes">
-                    <label>论文作者</label>
-                    <input type="text" class="form-control" id="paper_author" placeholder="论文作者" v-model="authors">
-                </div>
-                <!-- Paper conferences -->
-                <div class="form-group form-group-padding-fixes">
-                    <label>发布会议</label>
-                    <input type="text" class="form-control" id="paper_conf" placeholder="发布会议" v-model="conference">
-                </div>
-                <!-- Publish time -->
-                <div class="form-group form-group-padding-fixes">
-                    <label>发表时间</label>
-                    <div class="form-inline">
-                        <select class="form-control" id="year_select" v-model="year">
-                            <option>选择年份</option>
-                        </select>
-                        年
-                        <select class="form-control" v-model="month">
-                            <option>选择月份</option>
-                            <option>1</option>
-                            <option>2</option>
-                            <option>3</option>
-                            <option>4</option>
-                            <option>5</option>
-                            <option>6</option>
-                            <option>7</option>
-                            <option>8</option>
-                            <option>9</option>
-                            <option>10</option>
-                            <option>11</option>
-                            <option>12</option>
-                        </select>
-                        月
-                    </div>
-                </div>
-                <!-- Abstract -->
-                <div class="form-group form-group-padding-fixes">
-                    <label>论文摘要</label>
-                    <div class="textarea">
-                        <textarea type="" class="col-sm-12 form-control" v-model="abstract"></textarea>
-                    </div>
-                </div>
-                <!-- Paper PDF file -->
-                <div class="form-group form-group-padding-fixes">
-                    <label>选择文件</label>
-                    <input class="file-selector" type="file" id="file-selector">
-                </div>
-                <!-- Submit paper -->
-                <div class="form-group form-group-padding-fixes">
-                    <button id="paper-upload" class="btn btn-default" @click="upload()">提交</button>
-                </div>
-            </form>
+  <div class="row">
+    <div class="col-sm-1 col-md-1 col-lg-1"></div>
+    <div class="col-sm-10 col-md-10 col-lg-10">
+      <h1>{{language.upload_paper}}</h1>
+      <hr />
+      <form class="form-horizontal" role="form" @submit.prevent>
+        <!-- Paper title -->
+        <div class="form-group form-group-padding-fixes">
+          <label>{{language.paper_title}}</label>
+          <input type="text" class="form-control" id="paper_title" :placeholder="language.paper_title" v-model="title">
         </div>
-        <div class="hidden-sm hidden-md col-lg-1"></div>
+        <!-- Paper authors -->
+        <div class="form-group form-group-padding-fixes">
+          <label>{{language.author}}</label>
+          <input type="text" class="form-control" id="paper_author" :placeholder="language.author" v-model="authors">
+        </div>
+        <!-- Paper conferences -->
+        <div class="form-group form-group-padding-fixes">
+          <label>{{language.conference}}</label>
+          <input type="text" class="form-control" id="paper_conf" :placeholder="language.conference" v-model="conference">
+        </div>
+        <!-- Publish time -->
+        <div class="form-group form-group-padding-fixes">
+          <label>{{language.date}}</label>
+          <div class="form-inline">
+            <select class="form-control" id="year_select" v-model="year">
+              <option>{{language.choose_year}}</option>
+            </select>
+            {{language.year}}
+            <select class="form-control" v-model="month">
+              <option>{{language.choose_month}}</option>
+              <option>1</option>
+              <option>2</option>
+              <option>3</option>
+              <option>4</option>
+              <option>5</option>
+              <option>6</option>
+              <option>7</option>
+              <option>8</option>
+              <option>9</option>
+              <option>10</option>
+              <option>11</option>
+              <option>12</option>
+            </select>
+            {{language.month}}
+          </div>
+        </div>
+        <!-- Abstract -->
+        <div class="form-group form-group-padding-fixes">
+          <label>{{language.abstract}}</label>
+          <div class="textarea">
+            <textarea type="" class="col-sm-12 form-control" v-model="abstract"></textarea>
+          </div>
+        </div>
+        <!-- Paper PDF file -->
+        <div class="form-group form-group-padding-fixes">
+          <label>{{language.choose_file}}</label>
+          <input class="file-selector" type="file" id="file-selector">
+        </div>
+        <!-- Submit paper -->
+        <div class="form-group form-group-padding-fixes">
+          <button id="paper-upload" class="btn btn-default" @click="upload()">{{language.submit}}</button>
+        </div>
+      </form>
     </div>
+    <div class="hidden-sm hidden-md col-lg-1"></div>
+  </div>
 </div>
 </template>
 
@@ -81,93 +81,111 @@ import {unify_error} from "academia/util/error";
 import {msgbox} from "academia/util/core";
 
 export default {
-    data() {
-        return {
-            title: "",
-            authors: "",
-            abstract: "",
-            conference: "",
-            year: "选择年份",
-            month: "选择月份"
-        }
-    },
-    beforeRouteEnter: pre_route(login_required),
-    methods: {
-        init() {
-            let sel = $("#year_select");
-            const year_start = 2016;
-            for (let i = 0; i < 50; i++) {
-                sel.append(`<option>${year_start - i}</option>`);
-            }
-            console.log("added");
-        },
-        upload() {
-            console.log(`${this.title}, ${this.authors}`);
-
-            if (!this.check_input()) {
-              msgbox({
-                  type: "error",
-                  title: "无法上传论文",
-                  message: "您尚未填写笔记标题或内容。"
-              });
-              return;
-            }
-
-            let sel = $("#file-selector", this.$root.$el)[0];
-            console.log(sel);
-            if (sel && sel.files.length > 0) {
-                this.upload_file = sel.files[0];
-            } else {
-              console.log("bbb");
-              msgbox({
-                      type: "error",
-                      title: "无法上传论文",
-                      message: "你需要选择上传的文件。"
-                  });
-                return;
-            }
-
-            //Create upload transfer task
-            let new_upload_task = {
-                name: this.title,
-                type: "论文",
-                progress: 0,
-                transfered: 0,
-                total: 1
-            }
-            this.$root.upload_tasks.push(new_upload_task);
-
-            Paper.create({
-                title: this.title,
-                authors: this.authors,
-                abstract: this.abstract,
-                conference: this.conference,
-                // publish_date: this.publish_date(),
-                paper_file: this.upload_file
-            }, {
-                onUploadProgress: progress_listener(new_upload_task)
-            }).then((paper) => {
-                _.pull(this.$root.upload_tasks, new_upload_task);
-            });
-
-            this.$router.push("/");
-        },
-        publish_date() {
-            return `${this.year}-${this.month}`;
-        },
-        check_input() {
-            return this.title.length > 0 && this.authors.length > 0;
-        }
-    },
-    watch: {
-        $route: on_route_change
+  data() {
+    return {
+      title: "",
+      authors: "",
+      abstract: "",
+      conference: "",
+      year: "选择年份",
+      month: "选择月份",
+      language: {
+        paper_title: "",
+        authors: "",
+        conference: ""
+      }
     }
+  },
+  beforeRouteEnter: pre_route(login_required),
+  methods: {
+    init() {
+      let sel = $("#year_select");
+      const year_start = 2016;
+      for (let i = 0; i < 50; i++) {
+        sel.append(`<option>${year_start - i}</option>`);
+      }
+      // Setting language
+      let lang = this.$root.settings.lang;
+      this.language.upload_paper = lang == '#langCN' ? '上传论文' : 'Upload Paper';
+      this.language.paper_title = lang == '#langCN' ? '论文标题' : 'Paper title';
+      this.language.author = lang == '#langCN' ? '论文作者' : 'Paper Author';
+      this.language.conference = lang == '#langCN' ? '发布会议' : 'Conference';
+      this.language.date = lang == '#langCN' ? '发表时间' : 'Publish Date';
+      this.language.choose_year = lang == '#langCN' ? '选择年份' : 'Choose Year';
+      this.language.year = lang == '#langCN' ? '年' : 'Year';
+      this.language.choose_month = lang == '#langCN' ? '选择月份' : 'Choose Month';
+      this.language.month = lang == '#langCN' ? '月' : 'Month';
+      this.language.abstract = lang == '#langCN' ? '论文摘要' : 'Abstract';
+      this.language.choose_file = lang == '#langCN' ? '选择文件' : 'Choose File';
+      this.language.submit = lang == '#langCN' ? '提交' : 'Submit';
+    },
+    upload() {
+      console.log(`${this.title}, ${this.authors}`);
+
+      if (!this.check_input()) {
+        msgbox({
+          type: "error",
+          title: "无法上传论文",
+          message: "您尚未填写笔记标题或内容。"
+        });
+        return;
+      }
+
+      let sel = $("#file-selector", this.$root.$el)[0];
+      console.log(sel);
+      if (sel && sel.files.length > 0) {
+        this.upload_file = sel.files[0];
+      } else {
+        console.log("bbb");
+        msgbox({
+            type: "error",
+            title: "无法上传论文",
+            message: "你需要选择上传的文件。"
+          });
+        return;
+      }
+
+      //Create upload transfer task
+      let new_upload_task = {
+        name: this.title,
+        type: "论文",
+        progress: 0,
+        transfered: 0,
+        total: 1
+      }
+      this.$root.upload_tasks.push(new_upload_task);
+
+      Paper.create({
+        title: this.title,
+        authors: this.authors,
+        abstract: this.abstract,
+        conference: this.conference,
+        // publish_date: this.publish_date(),
+        paper_file: this.upload_file
+      }, {
+        onUploadProgress: progress_listener(new_upload_task)
+      }).then((paper) => {
+        _.pull(this.$root.upload_tasks, new_upload_task);
+      });
+
+      this.$router.push("/");
+    },
+    publish_date() {
+      return `${this.year}-${this.month}`;
+    },
+    check_input() {
+      return this.title.length > 0 && this.authors.length > 0;
+    }
+  },
+  watch: {
+    $route: on_route_change
+  }
 };
 </script>
 
 <style>
 textarea {
-    max-width: 100%;
+  max-width: 100%;
 }
 [class^='col-'] {
   padding-top: 10px;
